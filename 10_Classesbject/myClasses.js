ca7479d5aaaef6b59c2c0de63ff1f642c2acf978
sonu <sonugupta6045@gmail.com>
2025-06-06 16:05:44 +0530

    Add changeUsername method to user class and User prototype

--- 10_Classesbject/myClasses.js
+++ 10_Classesbject/myClasses.js
@@ -10,8 +10,34 @@ class user {
     encryptPassword(){
         return `${this.password}abc`
     }
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
 }
 
 const sonu = new user("user1","user@gmail.com","1234")
 
 console.log(sonu.encryptPassword());
+console.log(sonu.changeUsername());
+
+// behind the scene
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
+
