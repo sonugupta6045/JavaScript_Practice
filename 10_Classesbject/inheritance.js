e019ac342f9aff85e7d112cc60e8f774e6421c25
sonu <sonugupta6045@gmail.com>
2025-06-09 18:32:52 +0530

    Implement the Inheritance

---
+++ 10_Classesbject/inheritance.js
@@ -0,0 +1,25 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+    logMe(){
+        console.log(`USERNAME IS ${this.username}`);
+        
+    }
+}
+
+class Teacher extends User {
+    constructor(username,email,password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+        
+    }
+}
+
+const sonu = new Teacher("sonu1","sonu@gmail.com","123")
+sonu.addCourse()
